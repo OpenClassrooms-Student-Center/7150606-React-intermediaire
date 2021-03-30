4deb5cd621027405bc7d9f251dfcdd2ea651ffd2
Alexia Toulmet <alexia.toulmet@gmail.com>
2021-03-30 16:25:21 +0200

    P1C3 exercice branch change archi

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Home from './home'
-import Survey from './survey'
-import Results from './results'
-import Freelances from './freelances'
-import Header from './common/Header'
-import Error from './common/Error'
+import Home from './pages/Home'
+import Survey from './pages/Survey'
+import Results from './pages/Results'
+import Freelances from './pages/Freelances'
+import Header from './components/Header'
+import Error from './components/Error'
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/Freelances.jsx
+++ src/pages/Freelances.jsx
@@ -1,4 +1,4 @@
-import Card from './Card'
+import Card from '../components/Card'
 
 const freelanceProfiles = [
   {
