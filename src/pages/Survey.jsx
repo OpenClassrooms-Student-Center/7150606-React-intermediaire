be07ed586aca2c4d0ef8e517ec05940a40ee7347
Alexia Toulmet <alexia.toulmet@gmail.com>
2021-04-06 17:34:01 +0200

    P2C1 exercice loader

--- src/pages/Survey.jsx
+++ src/pages/Survey.jsx
@@ -1,22 +1,81 @@
+import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import colors from '../utils/style/colors'
+import { Loader } from '../utils/style/Atoms'
+
+const SurveyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const QuestionTitle = styled.h2`
+  text-decoration: underline;
+  text-decoration-color: ${colors.primary};
+`
+
+const QuestionContent = styled.span`
+  margin: 30px;
+`
+
+const LinkWrapper = styled.div`
+  padding-top: 30px;
+  & a {
+    color: black;
+  }
+  & a:first-of-type {
+    margin-right: 20px;
+  }
+`
 
 function Survey() {
   const { questionNumber } = useParams()
   const questionNumberInt = parseInt(questionNumber)
   const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1
   const nextQuestionNumber = questionNumberInt + 1
+  const [surveyData, setSurveyData] = useState({})
+  const [isDataLoading, setDataLoading] = useState(false)
+
+  // async function fetchData() {
+  //   try {
+  //     const response = await fetch(`http://localhost:8000/survey`)
+  //     const { surveyData } = await response.json()
+  //     setSurveyData(surveyData)
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
+
+  useEffect(() => {
+    // fetchData()
+    setDataLoading(true)
+    fetch(`http://localhost:8000/survey`).then((response) =>
+      response.json().then(({ surveyData }) => {
+        setSurveyData(surveyData)
+        setDataLoading(false)
+      })
+    )
+  }, [])
+
   return (
-    <div>
-      <h1>Questionnaire 🧮</h1>
-      <h2>Question {questionNumber}</h2>
-      <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
-      {questionNumberInt === 10 ? (
-        <Link to="/results">Résultats</Link>
+    <SurveyContainer>
+      <QuestionTitle>Question {questionNumber}</QuestionTitle>
+      {isDataLoading ? (
+        <Loader />
       ) : (
-        <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
+        <QuestionContent>{surveyData[questionNumber]}</QuestionContent>
       )}
-    </div>
+      <LinkWrapper>
+        <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
+        {surveyData[questionNumberInt + 1] ? (
+          <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
+        ) : (
+          <Link to="/results">Résultats</Link>
+        )}
+      </LinkWrapper>
+    </SurveyContainer>
   )
 }
 
--- src/utils/style/Atoms.jsx
+++ src/utils/style/Atoms.jsx
@@ -1,6 +1,24 @@
 import { Link } from 'react-router-dom'
-import styled from 'styled-components'
 import colors from './colors'
+import styled, { keyframes } from 'styled-components'
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+export const Loader = styled.div`
+  padding: 10px;
+  border: 6px solid ${colors.primary};
+  border-bottom-color: transparent;
+  border-radius: 22px;
+  animation: ${rotate} 1s infinite linear;
+`
 
 export const StyledLink = styled(Link)`
   padding: 10px 15px;
