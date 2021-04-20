3bc4312360d438a918c022feefb75a51dbb92ee1
Alexia Toulmet <alexia.toulmet@gmail.com>
2021-04-20 15:55:04 +0200

    change hooks location

--- src/utils/hooks/index.jsx
+++ src/utils/hooks/index.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext } from 'react'
-import { ThemeContext } from './context'
+import { ThemeContext } from '../context'
 
 export function useFetch(url) {
   const [data, setData] = useState()
