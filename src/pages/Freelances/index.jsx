74455d526ab0e857bb94ad72e6dab2f721ff04c0
Alexia Toulmet <alexia.toulmet@gmail.com>
2021-04-20 15:49:59 +0200

    change profile filename

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,5 +1,5 @@
 import Card from '../../components/Card'
-import DefaultPicture from '../../assets/default-profile.png'
+import DefaultPicture from '../../assets/profile.png'
 
 const freelanceProfiles = [
   {
