87d377ce864408ee48dce076ba2731ae96807061
Alexia Toulmet <alexia.toulmet@gmail.com>
2021-04-23 11:21:30 +0200

    P3C1 add fetParams tests

--- src/pages/Results/index.test.js
+++ src/pages/Results/index.test.js
@@ -1,4 +1,4 @@
-import { getJobTitle } from './'
+import { getJobTitle, getFetchParams } from './'
 
 describe('The getJobTitle function', () => {
   test('should add a comma to a word', () => {
@@ -8,5 +8,18 @@ describe('The getJobTitle function', () => {
   test('should not add a comma to the last element of the list', () => {
     const expectedState = 'item3'
     expect(getJobTitle('item3', 3, 2)).toEqual(expectedState)
+  })
+})
+
+describe('The getFetchParams function', () => {
+  test('should use the right format for param', () => {
+    const expectedState = 'a1=answer1'
+    expect(getFetchParams({ 1: 'answer1' })).toEqual(expectedState)
+  })
+  test('should concatenate params with an &', () => {
+    const expectedState = 'a1=answer1&a2=answer2'
+    expect(getFetchParams({ 1: 'answer1', 2: 'answer2' })).toEqual(
+      expectedState
+    )
   })
 })
