cfeafa57ce7621b1f89f379780edc94dfefea3df
Alexia Toulmet <alexia.toulmet@gmail.com>
2021-04-23 14:21:49 +0200

    P3C3 add test to card + delete com

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 import styled from 'styled-components'
 import colors from '../../utils/style/colors'
 import { useTheme } from '../../utils/hooks'
@@ -11,11 +12,14 @@ const CardLabel = styled.span`
   padding-left: 15px;
 `
 
-const CardTitle = styled.span`
+const CardTitle = styled.div`
   color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
   font-size: 22px;
   font-weight: normal;
   align-self: center;
+  height: 25px;
+  display: flex;
+  align-items: center;
 `
 
 const CardImage = styled.img`
@@ -42,12 +46,16 @@ const CardWrapper = styled.div`
 
 function Card({ label, title, picture }) {
   const { theme } = useTheme()
+  const [isFavorite, setIsFavorite] = useState(false)
+  const star = isFavorite ? '⭐️' : ''
 
   return (
-    <CardWrapper theme={theme} data-testid="yes">
+    <CardWrapper theme={theme} onClick={() => setIsFavorite(!isFavorite)}>
       <CardLabel theme={theme}>{label}</CardLabel>
       <CardImage src={picture} alt="freelance" />
-      <CardTitle theme={theme}>{title}</CardTitle>
+      <CardTitle theme={theme}>
+        {star} {title} {star}
+      </CardTitle>
     </CardWrapper>
   )
 }
--- src/pages/Freelances/index.test.js
+++ src/pages/Freelances/index.test.js
@@ -18,18 +18,13 @@ const freelancersMockedData = [
 ]
 
 const server = setupServer(
-  // On précise ici l'url qu'il faudra "intercepter"
   rest.get('http://localhost:8000/freelances', (req, res, ctx) => {
-    // Là on va pouvoir passer les datas mockées dans ce qui est retourné en json
     return res(ctx.json({ freelancersList: freelancersMockedData }))
   })
 )
 
-// Active la simulation d'API avant les tests
 beforeAll(() => server.listen())
-// Réinitialise tout ce qu'on aurait pu ajouter en termes de durée pour nos tests avant chaque test
 afterEach(() => server.resetHandlers())
-// Ferme la simulation d'API une fois que les tests sont finis
 afterAll(() => server.close())
 
 test('Should display freelancers names', async () => {
@@ -41,8 +36,3 @@ test('Should display freelancers names', async () => {
     screen.getByText('Hermione Granger')
   })
 })
-
-// 1- On veut vérifier que le loader s'affiche bien pendant le call
-// 2- Qu'on a autant de cards que de freelances dans la liste
-// 3- Que notre première card affiche bien les éléments récupérés dans le call avec le premier élément
-// 4- Vérifier erreurs
