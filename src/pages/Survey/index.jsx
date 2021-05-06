288885239262deafe25d429bf4526b5b91e653c1
Alexia Toulmet <alexia.toulmet@gmail.com>
2021-05-06 18:24:10 +0200

    switch to async await syntax

--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -37,17 +37,27 @@ function Survey() {
   const nextQuestionNumber = questionNumberInt + 1
   const [surveyData, setSurveyData] = useState({})
   const [isDataLoading, setDataLoading] = useState(false)
+  const [error, setError] = useState(false)
 
   useEffect(() => {
-    setDataLoading(true)
-    fetch(`http://localhost:8000/survey`).then((response) =>
-      response.json().then(({ surveyData }) => {
+    async function fetchSurvey() {
+      setDataLoading(true)
+      try {
+        const response = await fetch(`http://localhost:8000/survey`)
+        const { surveyData } = await response.json()
         setSurveyData(surveyData)
+      } catch (err) {
+        setError(err)
+      } finally {
         setDataLoading(false)
-      })
-    )
+      }
+    }
+    fetchSurvey()
   }, [])
 
+  if (error) {
+    return <span>Oups il y a eu un problème</span>
+  }
   return (
     <SurveyContainer>
       <QuestionTitle>Question {questionNumber}</QuestionTitle>
