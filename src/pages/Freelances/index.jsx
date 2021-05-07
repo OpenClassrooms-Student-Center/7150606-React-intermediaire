fb17eace987cd717fc73898ec506cf7b06601069
Alexia Toulmet <alexia.toulmet@gmail.com>
2021-05-07 11:44:45 +0200

    useFetch async await

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -43,7 +43,7 @@ function Freelances() {
   const freelancersList = data?.freelancersList
 
   if (error) {
-    return <pre>{error}</pre>
+    return <span>Il y a un problème</span>
   }
 
   return (
--- src/utils/hooks/index.jsx
+++ src/utils/hooks/index.jsx
@@ -2,26 +2,31 @@ import { useState, useEffect, useContext } from 'react'
 import { ThemeContext } from '../context'
 
 export function useFetch(url) {
-  const [data, setData] = useState()
+  const [data, setData] = useState({})
   const [isLoading, setLoading] = useState(true)
-  const [error, setError] = useState()
+  const [error, setError] = useState(false)
 
   useEffect(() => {
     if (!url) return
-    fetch(url)
-      .then((response) => response.json())
-      .then((jsonResponse) => setData(jsonResponse))
-      .then(() => setLoading(false))
-      .catch((err) => {
-        if (err) {
-          setError(err.toString())
-        }
-      })
+    setLoading(true)
+    async function fetchData() {
+      try {
+        const response = await fetch(url)
+        const data = await response.json()
+        setData(data)
+      } catch (err) {
+        setError(true)
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchData()
   }, [url])
   return { isLoading, data, error }
 }
 
 export function useTheme() {
   const { theme, toggleTheme } = useContext(ThemeContext)
+
   return { theme, toggleTheme }
 }
