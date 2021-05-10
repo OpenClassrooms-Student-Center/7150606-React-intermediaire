f0a8c11362f81665fee1f35e7b6e77e8e6c6d680
Alexia Toulmet <alexia.toulmet@gmail.com>
2021-05-10 18:51:10 +0200

    Add test fetchParams

--- src/pages/Results/index.test.js
+++ src/pages/Results/index.test.js
@@ -1,4 +1,4 @@
-import { formatJobList } from './'
+import { formatJobList, getFetchParams } from './'
 
 describe('The getJobTitle function', () => {
   it('should add a comma to a word', () => {
@@ -8,5 +8,18 @@ describe('The getJobTitle function', () => {
   it('should not add a comma to the last element of the list', () => {
     const expectedState = 'item3'
     expect(formatJobList('item3', 3, 2)).toEqual(expectedState)
+  })
+})
+
+describe('The getFetchParams function', () => {
+  it('should use the right format for param', () => {
+    const expectedState = 'a1=answer1'
+    expect(getFetchParams({ 1: 'answer1' })).toEqual(expectedState)
+  })
+  it('should concatenate params with an &', () => {
+    const expectedState = 'a1=answer1&a2=answer2'
+    expect(getFetchParams({ 1: 'answer1', 2: 'answer2' })).toEqual(
+      expectedState
+    )
   })
 })
