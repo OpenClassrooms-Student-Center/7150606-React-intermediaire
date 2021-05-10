59e3ad19acd5d5adc9db2d2279ff6249e15c3186
Alexia Toulmet <alexia.toulmet@gmail.com>
2021-05-10 10:03:01 +0200

    Change test function naming

--- src/pages/Results/index.test.js
+++ src/pages/Results/index.test.js
@@ -1,11 +1,11 @@
 import { formatJobList } from './'
 
 describe('The formatJobList function', () => {
-  test('should add a comma to a word', () => {
+  it('should add a comma to a word', () => {
     const expectedState = 'item2,'
     expect(formatJobList('item2', 3, 1)).toEqual(expectedState)
   })
-  test('should not add a comma to the last element of the list', () => {
+  it('should not add a comma to the last element of the list', () => {
     const expectedState = 'item3'
     expect(formatJobList('item3', 3, 2)).toEqual(expectedState)
   })
