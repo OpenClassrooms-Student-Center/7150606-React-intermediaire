e6cb19255a25a819178cde2c6f4f0db9b45bdf03
Alexia Toulmet <alexia.toulmet@gmail.com>
2021-05-18 17:34:57 +0200

    create balloon

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,29 @@
+import { useState } from 'react'
+import styled from 'styled-components'
+
+const HomeContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`
+
+const Balloon = styled.div`
+  height: 100px;
+  width: 100px;
+  border-radius: 50px;
+  background-color: #e20202;
+  transform: scale(${({ size }) => size});
+`
+
 function Home() {
+  const [size, setSize] = useState(1)
+
   return (
-    <div>
-      <h1> Page d'accueil 🏠</h1>
-    </div>
+    <HomeContainer>
+      <h1 onClick={() => setSize(size + 0.1)}> Page d'accueil 🏠</h1>
+      <Balloon size={size} />
+    </HomeContainer>
   )
 }
 
