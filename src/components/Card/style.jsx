2b638d7349ee9cbce74571a774e9809ec7994bc4
Alexia Toulmet <alexia.toulmet@gmail.com>
2021-05-20 11:12:32 +0200

    Styled Components in style file

---
+++ src/components/Card/style.jsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+import colors from '../../utils/style/colors'
+
+export const CardLabel = styled.span`
+  color: ${({ theme }) => (theme === 'light' ? colors.primary : '#ffffff')};
+  font-size: 22px;
+  font-weight: normal;
+  padding-left: 15px;
+`
+
+export const CardTitle = styled.div`
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
+  font-size: 22px;
+  font-weight: normal;
+  align-self: center;
+  height: 25px;
+  display: flex;
+  align-items: center;
+`
+
+export const CardImage = styled.img`
+  height: 150px;
+  width: 150px;
+  align-self: center;
+  border-radius: 50%;
+`
+
+export const CardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  padding: 15px;
+  background-color: ${({ theme }) =>
+    theme === 'light' ? colors.backgroundLight : colors.backgroundDark};
+  border-radius: 30px;
+  width: 300px;
+  height: 300px;
+  &:hover {
+    cursor: pointer;
+  }
+`
