3d95fd5a199837bbb595499bc3deb483056ad90e
Alexia Toulmet <alexia.toulmet@gmail.com>
2021-05-21 14:24:16 +0200

    test-id + queryParams

--- src/pages/Results/index.jsx
+++ src/pages/Results/index.jsx
@@ -52,7 +52,7 @@ const LoaderWrapper = styled.div`
   justify-content: center;
 `
 
-export function formatFetchParams(answers) {
+export function formatQueryParams(answers) {
   const answerNumbers = Object.keys(answers)
 
   return answerNumbers.reduce((previousParams, answerNumber, index) => {
@@ -73,7 +73,7 @@ export function formatJobList(title, listLength, index) {
 function Results() {
   const { theme } = useTheme()
   const { answers } = useContext(SurveyContext)
-  const fetchParams = formatFetchParams(answers)
+  const fetchParams = formatQueryParams(answers)
 
   const { data, isLoading, error } = useFetch(
     `http://localhost:8000/results?${fetchParams}`
