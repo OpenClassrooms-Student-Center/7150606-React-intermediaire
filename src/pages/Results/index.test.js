8b022a4ba56232ed689d981449ec867c5b7ffe86
Alexia Toulmet <alexia.toulmet@gmail.com>
2021-05-26 13:20:19 +0200

    Change queryParams naming

--- src/pages/Results/index.test.js
+++ src/pages/Results/index.test.js
@@ -1,4 +1,4 @@
-import { formatJobList, formatFetchParams } from './'
+import { formatJobList, formatQueryParams } from './'
 
 describe('The formatJobList function', () => {
   it('should add a comma to a word', () => {
@@ -11,14 +11,14 @@ describe('The formatJobList function', () => {
   })
 })
 
-describe('The formatFetchParams function', () => {
+describe('The formatQueryParams function', () => {
   it('should use the right format for param', () => {
     const expectedState = 'a1=answer1'
-    expect(formatFetchParams({ 1: 'answer1' })).toEqual(expectedState)
+    expect(formatQueryParams({ 1: 'answer1' })).toEqual(expectedState)
   })
   it('should concatenate params with an &', () => {
     const expectedState = 'a1=answer1&a2=answer2'
-    expect(formatFetchParams({ 1: 'answer1', 2: 'answer2' })).toEqual(
+    expect(formatQueryParams({ 1: 'answer1', 2: 'answer2' })).toEqual(
       expectedState
     )
   })
