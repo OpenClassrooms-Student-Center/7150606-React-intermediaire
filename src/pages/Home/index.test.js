53f7871bbef33abcd9ac6c99f6adcf7704f577d1
Alexia Toulmet <alexia.toulmet@gmail.com>
2021-05-26 15:49:11 +0200

    Delete error test

---
+++ src/pages/Home/index.test.js
@@ -0,0 +1,23 @@
+import { MemoryRouter } from 'react-router-dom'
+import { render, screen } from '@testing-library/react'
+import Home from './'
+import { ThemeProvider } from '../../utils/context'
+
+describe('The Home component', () => {
+  it('should render title', () => {
+    render(
+      <MemoryRouter>
+        <ThemeProvider>
+          <Home />
+        </ThemeProvider>
+      </MemoryRouter>
+    )
+    expect(
+      screen.getByRole('heading', {
+        level: 2,
+        text:
+          'Repérez vos besoins, on s’occupe du reste, avec les meilleurs talents',
+      })
+    ).toBeTruthy()
+  })
+})
