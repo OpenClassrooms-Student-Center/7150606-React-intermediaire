e81d274cbef747b8de141256cc124238b1d35450
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-11-20 11:07:15 +0100

    add switch and child routes to admin with users child

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Route, Switch, useRouteMatch } from 'react-router-dom'
 import Users from "./component/User"
 function Admin() {
-    const { url } = useRouteMatch() // renvoit le path actuel du composant Home
-    console.log("path actuel admin", url)
+    const { path, url } = useRouteMatch() // renvoit le path actuel du composant Home
+    console.log("path actuel admin", path)
     return <div>
         <h1>Administrateur</h1>
 
         <Switch>
 
-            <Route exact path={`${url}/users`}>
+            <Route exact path={`${path}/users`}>
                 <Users />
             </Route>
         </Switch>
