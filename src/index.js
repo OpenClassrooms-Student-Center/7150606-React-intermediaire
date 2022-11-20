0011a08fddd28e772a0014d205c8e82e2615d732
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-11-20 19:08:48 +0100

    index.js: add switch and component error

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './pages/Home'
 import Survey from './pages/Survey'
 import Question from './pages/Survey/component/Question'
 import Admin from './pages/Admin'
 import Header from './layouts/Header'
-
+import Error from "./components/Error"
 import './styles/index.css'
 import reportWebVitals from './reportWebVitals'
-
-
 
 //ReactDOM v18 ne supporte plus ReactDOM.render, il faut createRoot et root.render
 //const root = ReactDOM.createRoot(document.getElementById('root'))
@@ -19,26 +17,25 @@ ReactDOM.render(
   < React.StrictMode >
     <Router>
       <Header />
-      {/*Ajouter la prop exact pour que home s affiche sur ce path exactement et non sur un path contenant "/" , 
-c'est a dire que home ne doit pas s afficher dans le path du composant Survey qui commence par "/"*/}
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/survey">
-        <Survey />
-      </Route>
-      <Route path="/survey/:questionNumber">
-
-        <Question />
-
-      </Route>
-
-
-
-      <Route path="/admin">
-        <Admin />
-      </Route>
-
+      <Switch>
+        {/*Ajouter la prop exact pour que home s affiche sur ce path exactement et non sur un path contenant "/" , 
+  c'est a dire que home ne doit pas s afficher dans le path du composant Survey qui commence par "/"*/}
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/survey">
+          <Survey />
+        </Route>
+        <Route path="/survey/:questionNumber">
+          <Question />
+        </Route>
+        <Route path="/admin">
+          <Admin />
+        </Route>
+        <Route>
+          <Error />
+        </Route>
+      </Switch>
     </Router>
   </React.StrictMode >,
   document.getElementById('root')
