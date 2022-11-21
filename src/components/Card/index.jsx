be1ce3d83ac98be764c497917a2adddc5077da0f
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-11-21 17:54:43 +0100

    create  styled component CardLabel span element  and CardImage img element

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,13 +1,30 @@
 // import de PROPTYPES pour les props Card 
 import PropTypes from "prop-types"
+
+//import du styled de styled-components pour les composant style CSS in JS
+import styled from "styled-components"
+
 // import des profils et de l image par defaut de picture
 import defaultPictures from "../../assets/profil.png"
+
+//.........................................CREATION DES COMPOSANT STYLED..............................................................
+//les composant styled sont ecrit en Pascale Case comme les composant react
+const CardLabel = styled.span`
+color: #5843e4;
+font-size: 22px;
+font-weight: bold;
+`
+const CardImage = styled.img`
+    height: 80px;
+    width: 80px;
+    border-radius: 50%;
+`
 
 function Card({ label, title, picture }) {
     return (
         <div style={{ display: 'flex', flexDirection: 'column', padding: 15 }}>
-            <span>{label}</span>
-            <img src={picture} alt="freelance" height={100} width={100} />
+            <CardLabel>{label}</CardLabel>
+            <CardImage src={picture} alt="freelance" />
             <span>{title}</span>
         </div>
     )
