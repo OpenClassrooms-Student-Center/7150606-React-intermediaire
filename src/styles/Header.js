40f709ecdf286e99bb680657aeef0798dacda861
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-11-22 11:23:20 +0100

    styles/header.js: fix prop  StyledLink

--- src/styles/Header.js
+++ src/styles/Header.js
@@ -4,8 +4,18 @@ import styled from "styled-components"
 // creation d un styled composant avec un element qui n existe pas dans le DOM mais avec un element Link qui provient d une bibliotheque "react router dom"
 //passé en parametre de styled
 const StyledLink = styled(Link)`
-   
-`
+    padding: 10px;
+    color: #8186a0;
+    text-decoration: none;
+    font-size: 18px;
+    display: flex;
+
+    ${(props) =>
+        props.$isFullLink &&
+        `color: white;
+        border-radius: 30px;
+        background-color: #5843E4; `}
+    `
 const StyledHeader = styled.header`
     display:flex;
 
@@ -21,21 +31,7 @@ const StyledHeader = styled.header`
             width: 50%;
         
         }
-        ${StyledLink} {
-            padding: 10px;
-            color: #8186a0;
-            text-decoration: none;
-            font-size: 18px;
-            display: flex;
-            
-            ${(props) =>
-        props.$isFullLink &&
-        ` color: white;
-                border-radius: 30px;
-                background-color: #5843E4; `}
-
-        }
-    
+      
     }
 `
 
