ec538c3cb9aff4c59aa0ef30280bb89514b91abb
aurelien-oc-mentorship <65898532+aurelien-oc-mentorship@users.noreply.github.com>
2023-04-13 21:22:40 -0500

    Update React Router V6

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Home from './pages/Home'
-import Survey from './pages/Survey'
-import Results from './pages/Results'
-import Freelances from './pages/Freelances'
-import Header from './components/Header'
-import Error from './components/Error'
-import { createGlobalStyle } from 'styled-components'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Survey from './pages/Survey';
+import Results from './pages/Results';
+import Freelances from './pages/Freelances';
+import Header from './components/Header';
+import Error from './components/Error';
+import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
     * {
@@ -17,30 +17,21 @@ const GlobalStyle = createGlobalStyle`
     body {
       margin: 0;
     }
-`
+`;
+
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <GlobalStyle />
       <Header />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/survey/:questionNumber">
-          <Survey />
-        </Route>
-        <Route path="/results">
-          <Results />
-        </Route>
-        <Route path="/freelances">
-          <Freelances />
-        </Route>
-        <Route>
-          <Error />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/survey/:questionNumber" element={<Survey />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/freelances" element={<Freelances />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
